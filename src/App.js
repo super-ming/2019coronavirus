b03f09b671fce2e119322edae041430c1bb09f5d
Ming Ho <hkboi108@hotmail.com>
2020-03-01 00:32:12 -0800

    Add header hamburger button icon

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 import Homepage from './components/pages/homepage';
 
-import { DataReportProvider } from './components/context/DataReportContext';
-
 function App() {
-  const [dataReport, setDataReport] = useState({});
-  const loadDataReport = () => {
-    fetch('http://localhost:5000/api/data').then(res =>res.json())
-    .then(data => {
-      setDataReport(data);
-    }).catch(err => console.log(err))
-  };
-  useEffect(() => loadDataReport(), [])
-
+  
   return (
     <div className="App">
-      <header>
-      
-      </header>
-      <DataReportProvider value={dataReport}>
-        <Homepage />
-      </DataReportProvider>
-      
+      <Homepage />
     </div>
   );
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
+import { ReactComponent as Icon } from '../assets/buttonicon.svg';
 
 function Header(props){
     return (
         <div className="flex flex-row w-full bg-blue-500 ">
-            <button className="mx-4" onClick={()=>props.toggleList()}>Button</button>
-            <div className="text-3xl">Header</div>
+            <button className="mx-4" onClick={()=>props.toggleList()}><Icon width={25}/></button>
+            <div className="text-3xl">COVID-19 Tracker</div>
         </div>
     )
 }
