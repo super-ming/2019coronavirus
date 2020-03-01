1503c5e3dcc1f1b9b4c705e66242fe1e4feb41b3
Ming Ho <hkboi108@hotmail.com>
2020-03-01 00:31:13 -0800

    Add load data on mount effect and map centering on list click

--- src/components/map.js
+++ src/components/map.js
@@ -3,22 +3,57 @@ import { Map, CircleMarker, TileLayer } from 'react-leaflet';
 import "leaflet/dist/leaflet.css";
 import DataReportContext from './context/DataReportContext';
 
-function LeafletMap(){
+function LeafletMap(props){
     const data = useContext(DataReportContext);
     const makeMarkers = () => {
-        if(data.casesByCountry){
-            const markers = data.casesByCountry.map((c, idx) => {
-                return (<CircleMarker key={idx} center={[c.lat, c.lng]}/>)
+        if(data.cases){
+            const markers = data.cases.map((c, idx) => {
+                if(c.lat && c.lng){
+                    return (<CircleMarker 
+                        key={idx} center={[c.lat, c.lng]} 
+                        radius={c.confirmedCount < 2 ? 2 : 2 * Math.log(c.confirmedCount)}
+                        fillRadius={1}
+                        stroke={false}
+                        fillOpacity={0.8}
+                        />
+                    )
+                }
+                
             })
             return markers
         }
     };
+    let minLat, minLng, maxLat, maxLng, centerLat, centerLng, bufferLat, bufferLng;
+    
+    if(data.minMaxLatLng){
+        minLat = data.minMaxLatLng.minLat;
+        minLng = data.minMaxLatLng.minLng;
+        maxLat = data.minMaxLatLng.maxLat;
+        maxLng = data.minMaxLatLng.maxLng;
+        centerLat = (minLat + maxLat) / 2;
+        const distanceLat = maxLat - minLat;
+        bufferLat = distanceLat * 0.05;
+        centerLng = (minLng + maxLng) / 2;
+        const distanceLng = maxLng - minLng;
+        bufferLng = distanceLng * 0.15;
+    }
+    if(props.currentLocation){
+        const loc = data.casesByCountry.find(el => el._id === props.currentLocation);
+        centerLat = loc.lat;
+        centerLng = loc.lng;
+    }
+    
     return (
-        <div className="w-3/4">
+        <div className="w-1/2 p-4">
             <Map
                 style={{ height: "480px", width: "100%" }}
-                zoom={1}
-                center={[-0.09, 51.505]}>
+                zoom={5}
+                center={props.currentLocation ? [centerLat, centerLng] : [-0.09, 51.505]}
+                bounds={minLat ? [
+                    [minLat - bufferLat, minLng - bufferLng],
+                    [maxLat + bufferLat, maxLng + bufferLng]
+                  ] : [[24,72],[47,121]]}
+            >
                 <TileLayer url="http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
                 {data && makeMarkers()}
             </Map>
--- src/components/pages/homepage.js
+++ src/components/pages/homepage.js
@@ -1,24 +1,36 @@
-import React, { useEffect, useState, useContext, Fragment } from 'react';
-import DataReportContext from '../context/DataReportContext';
-import List from '../list';
+import React, { useEffect, useState, useContext, Fragment, lazy, Suspense } from 'react';
+import Utils from '../../utils/utils';
 import LeafletMap from '../map';
 import Header from '../header';
+import { DataReportProvider } from '../context/DataReportContext';
+const List = lazy(() => import('../list'));
 
 function Homepage() {
-    const data = useContext(DataReportContext);
-    
     const [openList, setOpenList] = useState(true);
+    const [listLocation, setListLocation] = useState("");
+    const [dataReport, setDataReport] = useState({});
+    const loadDataReport = () => {
+        Utils.getData('http://localhost:5000/api/data').then(data => {
+            setDataReport(data);
+        }).catch(err => console.log(err))
+    };
+    useEffect(() => loadDataReport(), [])
     function toggleList(){
         setOpenList(!openList);
     }
+    function handleListClick(e){
+        setListLocation(e);
+    }
     return (
-        <Fragment>
+        <DataReportProvider value={dataReport}>
             <Header toggleList={toggleList}/>
-            <div className={"flex flex-row h-screen"}>
-                <List data={data} openList={openList}/> 
-                <LeafletMap />  
+            <div className={"flex flex-row h-screen bg-blue-200"}>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <List openList={openList} handleListClick={handleListClick}/> 
+                </Suspense>
+                <LeafletMap currentLocation={listLocation}/>  
             </div>
-        </Fragment>
+        </DataReportProvider>
     )
 }
 
