2721fcc5a68390ed9154968bb59938fb3c01bdb0
Ming Ho <hkboi108@hotmail.com>
2020-03-08 18:36:24 -0700

    Refactor list component

--- src/components/list.js
+++ src/components/list.js
@@ -1,28 +1,16 @@
-import React, { useEffect, useState, useContext, Fragment } from 'react';
+import React, { useContext, Fragment } from 'react';
 import Utils from '../utils/utils';
 import DataReportContext from './context/DataReportContext';
 
 function List(props){
     const data = useContext(DataReportContext);
-	const [totalConfirmed, setTotalConfirmed] = useState();
-	const [loading, setLoading] = useState(true);
-	useEffect(()=>{
-		if(data.cases){
-			setLoading(false);
-			const total = data.cases.reduce((acc, cur) => {
-				const confirmed = cur.confirmedCount;
-				return acc += parseInt(confirmed); 
-			}, 0);
-			setTotalConfirmed(total);
-		}
-	}, [data.cases]);
+
 	const makeList = () => {
 		if(data.casesByCountry){
 			const list = data.casesByCountry.map((c, idx) => {
-				
 				const {_id, confirmedCount, deathCount, recoveredCount} = c;
 				return (
-					<div key={idx} className="bg-yellow-400 hover:bg-white p-4" data-name={_id} onClick={(e)=>props.handleListClick(e.currentTarget.getAttribute("data-name"))}>
+					<div key={idx} className="bg-yellow-400 hover:bg-white p-4 border-b" data-name={_id} onClick={(e)=>props.handleListClick(e.currentTarget.getAttribute("data-name"))}>
 						<div className="flex flex-row">
 							<div className={"text-base text-blue-700"}>{Utils.formatNum(confirmedCount)}</div>
 							<div className={"text-base pl-1"}>{_id}</div>
@@ -39,18 +27,12 @@ function List(props){
     
 	return (
 		<Fragment>
-			{loading && <div className="animated pulse infinite h-full pt-32 pl-4">Loading...</div>}
 			{data.casesByCountry && 
-				<div className={`w-1/5 overflow-auto transition ease-in-out animated ${props.openList ? `slideInLeft` : `animated slideOutLeft`} `}>
-					<div className="flex flex-row bg-yellow-500 hover:bg-white border-b">
-						<div className="pl-4 text-xl text-blue-700 py-4">{totalConfirmed}</div>
-						<div className="w-full text-lg text-center py-4">Confirmed cases</div>
-					</div>
+				<div className="overflow-scroll">
 					{makeList()}
 				</div>
 			}
 		</Fragment>
-		
 	)
 }
 
