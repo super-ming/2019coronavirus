0a16d94e7340349b9b714fb8e1eeff00204fb7a0
Ming Ho <hkboi108@hotmail.com>
2020-03-08 18:34:21 -0700

    Add Twitter component

---
+++ src/components/TwitterBoard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function TwitterBoard(props){
+    return (
+        <div className="flex flex-row w-1/4 h-40 bg-white hover:bg-white border-b rounded">
+            Twitter
+        </div>
+    )
+};
+
+export default TwitterBoard;
