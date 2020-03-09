3a6b9659e718f7879a2f41d4ba2423f5529d4c1e
Ming Ho <hkboi108@hotmail.com>
2020-03-08 18:34:59 -0700

    Add footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer(){
+    return (
+        <div className="flex flex-row w-full bg-blue-600 hover:text-white ">
+            Source: <a href="https://systems.jhu.edu/research/public-health/ncov/">John Hopkins University Center for Systems Science and Engineering </a>
+        </div>
+    )
+}
+
+export default Footer;
